36ca5e34fb55835107d600c5d998e12f8794b145
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-18 22:05:42 +0300

    feat: add send form

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
-
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
-
-// node app.js Tom 23
+const express = require("express");
+   
+const app = express();
+   
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = express.urlencoded({extended: false});
+  
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+app.post("/", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+   
+app.listen(3000, ()=>console.log("Сервер запущен..."));
